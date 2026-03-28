500483dbbd7210da3db215b2e2d4b75e0adea751
kwotaq <PJXORISJ@gmail.com>
2026-03-28 04:00:43 +0200

    switch to gevent

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 OsuStreamService().init_app(socketio)
 app.register_blueprint(players_bp)
 app.register_blueprint(scores_bp)
